1ffaea7eaac523e3ec1071b6bf94e16764cf7d02
rabchi-dk <rabchi@proton.me>
2023-12-15 18:46:36 +0100

    2023: Day 12 (move and rename stuff)

--- 2023/12/app_part2.rb
+++ 2023/12/app_part2.rb
@@ -2,7 +2,7 @@ require_relative 'solver'
 
 input = File.read("input_challenge.txt")
 
-solver = Solver.new
-result = solver.solve_part2(input)
+solver = SolverPart2.new
+result = solver.solve(input)
 puts "Part 2:"
 puts result
