0c6d9fac452705357223bb9b31c66b0342797cf5
rabchi-dk <rabchi@proton.me>
2023-12-18 14:37:14 +0100

    [WIP] 2023: Day 12 (use counting automata)

--- 2023/12/app_part2.rb
+++ 2023/12/app_part2.rb
@@ -2,7 +2,7 @@ require_relative 'solver'
 
 input = File.read("input_challenge.txt")
 
-solver = SolverPart2.new
+solver = SolverPart2CountingAutomata.new
 result = solver.solve(input)
 puts "Part 2:"
 puts result
